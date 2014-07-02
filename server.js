1aaa07280def10f68cc592f48a2cbbe1b64cc4eb
Andy Potanin <andy.potanin@usabilitydynamics.com>
2014-07-02 13:17:27 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-
 /**
+
  * Module dependencies.
  */
 
@@ -15,22 +15,14 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
 
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+app.use( function( req, res ) {
+  res.send( 404, 'nothing here' );
+});
+
+app.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
